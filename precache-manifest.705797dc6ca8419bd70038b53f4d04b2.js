1ea29e4979bdc19d0ebff492268d54b5a3b9d53a
Phillip Chapados <pchapados@ieee.org>
2020-04-24 12:20:12 -0400

    Updates

---
+++ precache-manifest.705797dc6ca8419bd70038b53f4d04b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f179de783158d8d4adc524446d1e034c",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "f7b863f7dcbba5187778",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9bec81a04ba7fa85b56a",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f7b863f7dcbba5187778",
+    "url": "/contactmanager/static/js/2.ace55220.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.ace55220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bec81a04ba7fa85b56a",
+    "url": "/contactmanager/static/js/main.5bcb545f.chunk.js"
+  },
+  {
+    "revision": "2b43dc7d0ef5c78fcd0f",
+    "url": "/contactmanager/static/js/runtime-main.08b2bbf7.js"
+  }
+]);
